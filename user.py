ea4666f6c4d3e26590daf9940e5b617aadb8a407
akshay bogar <akshaymbogar0099@gmail.com>
2019-07-19 00:59:00 +0530

    user object mapped from the database using username

--- user.py
+++ user.py
@@ -1,5 +1,22 @@
+import sqlite3
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+        select_query = 'SELECT * FROM USERS WHERE username = ?'
+        result = cursor.execute(select_query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        conn.close()
+        return user
