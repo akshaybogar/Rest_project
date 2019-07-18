3408937b2ea49cbbcc298ff73b8234dd5aab57a6
akshay bogar <akshaymbogar0099@gmail.com>
2019-07-19 01:01:34 +0530

    user object mapped from database using id

--- user.py
+++ user.py
@@ -20,3 +20,18 @@ class User:
 
         conn.close()
         return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+        select_query = 'SELECT * FROM USERS WHERE id = ?'
+        result = cursor.execute(select_query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        conn.close()
+        return user
