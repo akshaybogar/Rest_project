0d29f3c4039d54932b253f445cc5795a1f232a19
akshay bogar <akshaymbogar0099@gmail.com>
2019-07-19 23:57:43 +0530

    Adding checks before creating a table

--- table.py
+++ table.py
@@ -4,18 +4,8 @@ conn = sqlite3.connect('data.db')
 
 cursor = conn.cursor()
 
-create_query = 'CREATE TABLE USERS(id int, username text, password text)'
+create_query = 'CREATE TABLE IF NOT EXISTS USERS(id INTEGER PRIMARY KEY, username text, password text)'
 conn.execute(create_query)
-
-user = (1, 'john', 'asdf')
-insert_query = 'INSERT INTO USERS VALUES(?, ?, ?)'
-conn.execute(insert_query, user)
-
-users = [
-(2, 'adam', 'ghjk'),
-(3, 'eve', 'zxcv')
-]
-conn.executemany(insert_query, users)
 
 conn.commit()
 conn.close()
