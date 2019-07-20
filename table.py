13a90b620b16cceb70c6dc71e86cbf96cf211783
akshay bogar <akshaymbogar0099@gmail.com>
2019-07-20 10:17:45 +0530

    storing items in the items table for testing

--- table.py
+++ table.py
@@ -11,6 +11,7 @@ create_query = 'CREATE TABLE IF NOT EXISTS ITEMS(name text, price real)'
 cursor.execute(create_query)
 
 insert_query = 'INSERT INTO ITEMS VALUES("test_item", 9.99)'
+cursor.execute(insert_query)
 
 conn.commit()
 conn.close()
